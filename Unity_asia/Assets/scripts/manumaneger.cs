9171421f2f774eceeba65993bf155fdcb28c87cf
stiffgg <ch003924@gmail.com>
2020-11-03 16:03:56 +0800

    11/03
    123123

---
+++ Unity_asia/Assets/scripts/manumaneger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public void gamestart()
+    {
+        print("start");
+        SceneManager.LoadScene("Lv1");
+
+    }
+
+    public void quit ()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+
+        
+    }
+
+
+}
